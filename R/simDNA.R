c2cde8b127294ac65565a95335cd0395066153c8
Maiken Bak Poulsen <maikenbak@hotmail.com>
2019-11-18 14:24:12 +0100

    Made package help-file

---
+++ R/simDNA.R
@@ -0,0 +1,28 @@
+#' The simDNA package
+#'
+#' @description
+#' R functions for simulating and analyzing DNA sequences.
+#'
+#' @details
+#' Contains function to simulate segregating sites matrices for various population types.
+#' It also contains functions to analyze DNA based on segregating sites matrices or the site
+#' frequency spectrum.
+#'
+#' The names of these functions are the following
+#' \itemize{
+#'    \item \code{\link{mutRate}}
+#'    \item \code{\link{pairwDiff}}
+#'    \item \code{\link{print.DNAmat_class}}
+#'    \item \code{\link{SFS}}
+#'    \item \code{\link{simDNAseq}}
+#'    \item \code{\link{SNP}}
+#'    \item \code{\link{TajimaD}}
+#' }
+#'
+#' @author
+#' Elias Arildskov, Camilla P. Kudahl and Maiken B. Poulsen.
+#'
+#' @docType package
+#' @name simDNA
+
+NULL
